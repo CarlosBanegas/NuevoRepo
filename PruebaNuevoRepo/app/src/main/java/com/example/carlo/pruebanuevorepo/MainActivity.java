54a340a08ef16131212c6b4cadf3875ca0145f82
josseamaya <josseamaya@gmail.com>
2016-03-09 16:35:47 -0600

    agregue mas texto
    mi nombre es app en la telview text

--- PruebaNuevoRepo/app/src/main/java/com/example/carlo/pruebanuevorepo/MainActivity.java
+++ PruebaNuevoRepo/app/src/main/java/com/example/carlo/pruebanuevorepo/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TextView tv=(TextView)findViewById(R.id.textHola);
-        tv.setText("mucho gusto");
+        tv.setText("mucho gusto mi nombre es apk");
 
     }
 
